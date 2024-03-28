3a5366b62fac97ebb595b5119903e748b42bfd0d
Duong Thanh Truong <dttruonga8tqtpy@gmail.com>
2024-03-28 22:14:59 +0700

    create Statistic with time, department, branch

--- src/main/java/View/ThietBi/DeviceView.java
+++ src/main/java/View/ThietBi/DeviceView.java
@@ -1,7 +1,11 @@
 package View.ThietBi;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class DeviceView extends JPanel{
-
+	public DeviceView() {
+		this.setBackground(Color.red);
+	}
 }

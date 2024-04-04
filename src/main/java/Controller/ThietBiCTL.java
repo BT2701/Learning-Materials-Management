ccd07651647778ee13c1d671be7bf8f0329782b1
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-04-04 11:04:05 +0700

    upload handle device

--- src/main/java/Controller/ThietBiCTL.java
+++ src/main/java/Controller/ThietBiCTL.java
@@ -16,14 +16,46 @@ public class ThietBiCTL {
 	public ThietBiModel getModel(int id) {
 		return dal.getModel(id);
 	}
-	public void addModel(ThietBiModel device) {
-		dal.addModel(device);
+	public String addModel(ThietBiModel device) {
+            if(dal.addModel(device)) {
+                return "Thêm thành công";
+            } else return "Thêm thất bại";
 	}
-	public void updateModel(ThietBiModel device) {
-		dal.updateModel(device);
+	public String updateModel(ThietBiModel device) {
+            if(dal.updateModel(device)) {
+                return "Sửa thành công";
+            } else return "Sửa thất bại";
 	}
-	public void deleteModel(int id) {
-		ThietBiModel x=getModel(id);
-		dal.deleteModel(x);
+	public String deleteModel(int id) {
+            ThietBiModel x = getModel(id);
+            if(dal.deleteModel(x)) {
+                  return "Xóa thành công";
+            } else return "Xóa thất bại";
 	}
+        
+        public int getNewestID() {
+            ArrayList<ThietBiModel> list = dal.getList();
+            int len = list.size();
+            int id = list.get(len - 1).getMaTB();
+            return id + 1;
+        }
+        
+        public String addListModel(ArrayList<ThietBiModel> list) {
+            for(ThietBiModel thietBiModel: list) {
+                if(dal.getModel(thietBiModel.getMaTB()) != null) {
+                    return "Đã có mã thiết bị " + thietBiModel.getMaTB() + " trong dữ liệu";
+                }
+            }
+            
+            if(dal.addListModel(list)) {
+                return "Thêm danh sách thành công";
+            } else return "Thêm danh sách thất bại";
+            
+        }
+        
+        public String deleteListModel(ArrayList<ThietBiModel> list) {
+            if(dal.deleteListModel(list)) {
+                return "Xóa thành công";
+            } else return "Xóa thất bại";           
+        }
 }
